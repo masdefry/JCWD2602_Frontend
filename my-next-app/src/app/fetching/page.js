6f6de46f55fa89db7984ffbb3e360dcd4d3c1d3c
masdefry <muhammaddefryan@gmail.com>
2023-11-27 11:51:25 +0700

    Sequential Data Fetching

--- my-next-app/src/app/fetching/page.js
+++ my-next-app/src/app/fetching/page.js
@@ -2,8 +2,13 @@ import { urlApi } from "./../../utils/urlApi.js";
 
 export default async function Fetching(){
 
+    // cache: 'force-cache' ---> SSG
+    // cache: 'no-store' ---> SSR
+    // next: { revalidate: 20 } ---> ISR
+
     const response = await fetch(`${urlApi}/products`, {
-        method: 'GET'
+        method: 'GET',
+        // cache: 'no-store'
     })
     const data = await response.json()
 
