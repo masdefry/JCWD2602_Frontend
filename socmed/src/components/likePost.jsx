8cce49114f67082cc77e455d8bac7fb4a2b3e306
masdefry <muhammaddefryan@gmail.com>
2023-12-04 08:55:49 +0700

    Day15 Done

--- socmed/src/components/likePost.jsx
+++ socmed/src/components/likePost.jsx
@@ -3,18 +3,17 @@ import { IoMdHeart } from "react-icons/io";
 import { useMutation } from "@tanstack/react-query";
 
 export default async function LikePost(props){
-    useMutation({
-        mutationFn: async() => {
-            await fetch(`http://localhost:5000/posts/${props.id}`, {
-                method: 'PATCH', 
-                body: 
-            })
-        }
-    })
+    // useMutation({
+    //     mutationFn: async() => {
+    //         await fetch(`http://localhost:5000/posts/${props.id}`, {
+    //             method: 'PATCH', 
+    //             body: 
+    //         })
+    //     }
+    // })
 
     return(
         <>
-            {props.id}
             <IoMdHeart className='text-3xl text-red-400' />
         </>
     )
